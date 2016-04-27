b3180d010c8baffa1c26c528dd287ff8bb17865a
Jordi Pijuan <jordipijuan@informaticos.com>
2016-04-27 21:53:24 +0200

    Rename namespace

---
+++ Schibsted.Infrastructure.Contracts/IUsersRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Schibsted.Infrastructure.Contracts
+{
+
+    public interface IUsersRepository<T> : IReaderRepository<T> where T: class
+    {
+        void Initialize(string filepath);
+    }
+
+}
---
+++ Schibsted.Infrastructure.Repositories/RolesRepository.cs
@@ -0,0 +1,36 @@
+﻿namespace Schibsted.Infrastructure.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using Entities;
+
+    public class RolesRepository : IRolesRepository<Role>
+    {
+        private string _File;
+        public string File
+        {
+            get { return _File; }
+            set { _File = value; }
+        }
+
+        public void Initialize(string filepath)
+        {
+            File = filepath;
+        }
+
+        public IEnumerable<Role> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Role> GetByFilter(Func<Role, bool> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Role GetById(object id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
